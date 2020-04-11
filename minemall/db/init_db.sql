f9f51b90cb8d21d249bcdd9cafa0b98c835af48b
jay <22>
2020-04-11 15:29:20 +0800

    添加配置文件

---
+++ minemall/db/init_db.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS mall_goods;
+CREATE DATABASE mall_goods DEFAULT CHARSET utf8mb4 collate utf8mb4_general_ci;
+
+DROP DATABASE IF EXISTS mall_order;
+CREATE DATABASE mall_order DEFAULT CHARSET utf8mb4 collate utf8mb4_general_ci;
+
+DROP DATABASE IF EXISTS message_confirm;
+CREATE DATABASE message_confirm DEFAULT CHARSET utf8mb4 collate utf8mb4_general_ci;
+
+DROP DATABASE IF EXISTS resource_manage;
+CREATE DATABASE resource_manage DEFAULT CHARSET utf8mb4 collate utf8mb4_general_ci;
+
+DROP DATABASE IF EXISTS sc_admin;
+CREATE DATABASE sc_admin DEFAULT CHARSET utf8mb4 collate utf8mb4_general_ci;
+
+DROP DATABASE IF EXISTS sc_auth;
+CREATE DATABASE sc_auth DEFAULT CHARSET utf8mb4 collate utf8mb4_general_ci;
+
+DROP DATABASE IF EXISTS sc_gateway;
+CREATE DATABASE sc_gateway DEFAULT CHARSET utf8mb4 collate utf8mb4_general_ci;
