beed012fbead75e1879e93304c9506dc5b520721
Young-Guiji <2053106420@qq.com>
2020-04-13 20:36:03 +0800

    修改sql

--- gateway/gateway-admin/src/main/db/ddl/mysql/gateway.sql
+++ gateway/gateway-admin/src/main/db/ddl/mysql/gateway.sql
@@ -10,10 +10,13 @@ CREATE TABLE gateway_route
   orders       INT COMMENT '排序',
   description  VARCHAR(500) COMMENT '描述',
   status       VARCHAR(1)            DEFAULT 'Y' COMMENT '状态：Y-有效，N-无效',
-  created_time DATETIME     NOT NULL DEFAULT now() COMMENT '创建时间',
-  updated_time DATETIME     NOT NULL DEFAULT now() COMMENT '更新时间',
-  created_by   VARCHAR(100) NOT NULL COMMENT '创建人',
-  updated_by   VARCHAR(100) NOT NULL COMMENT '更新人'
+  created_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  updated_time datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '更新时间',
+  created_id varchar(20) CHARACTER SET utf8 DEFAULT NULL COMMENT '创建人id',
+  created_by varchar(100) NOT NULL COMMENT '创建人',
+  updated_id varchar(20) CHARACTER SET utf8 DEFAULT NULL COMMENT '更新人ID',
+  pdated_by varchar(100) NOT NULL COMMENT '更新人',
+  version int(11) DEFAULT '0'
 ) COMMENT '网关路由表';
 
 CREATE UNIQUE INDEX ux_gateway_routes_uri ON gateway_route (uri);
